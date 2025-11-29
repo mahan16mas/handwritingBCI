617e7ef43a8bfec8ae55bcfd22049398558f73dd
mahan <mahan.48415@gmail.com>
2025-11-29 20:57:46 +0330

    l

--- charSeqRNN.py
+++ charSeqRNN.py
@@ -383,8 +383,8 @@ class charSeqRNN(object):
         self.sess.graph.finalize()
             
         while i < self.args['nBatchesToTrain']:
-            print('*'*30)
-            print(i)
+            # print('*'*30)
+            # print(i)
             #time how long this batch takes
             dtStart = datetime.now()
 
