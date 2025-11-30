4669b558606a3c1ea11ecd7803e326bb9f651255
mahan <mahan.48415@gmail.com>
2025-11-30 22:52:38 +0330

    l

--- Step5_rnnInference.py
+++ Step5_rnnInference.py
@@ -22,7 +22,7 @@ dataDirs = ['t5.2019.05.08','t5.2019.11.25','t5.2019.12.09','t5.2019.12.11','t5.
             't5.2019.12.20','t5.2020.01.06','t5.2020.01.08','t5.2020.01.13','t5.2020.01.15']
 
 #use this train/test partition
-cvPart = 'HeldOutTrials'
+cvPart = 'HeldOutBlocks55'
 
 #point this towards the specific RNN we want to evaluate
 rnnOutputDir = cvPart
