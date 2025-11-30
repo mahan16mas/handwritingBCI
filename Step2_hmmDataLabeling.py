ceb7354868497adcdc711fa84c559e81785df986
mahan <mahan.48415@gmail.com>
2025-11-30 22:40:29 +0330

    l

--- Step2_hmmDataLabeling.py
+++ Step2_hmmDataLabeling.py
@@ -17,7 +17,7 @@ rootDir = "/data/hossein/mm_project/" + '/handwritingBCIData/'
 
 #define which datasets to process
 dataDirs = ['t5.2019.05.08','t5.2019.11.25','t5.2019.12.09','t5.2019.12.11','t5.2019.12.18',
-            't5.2019.12.20','t5.2020.01.06','t5.2020.01.08','t5.2020.01.13','t5.2020.01.15']
+            't5.2019.12.20','t5.2020.01.06','t5.2020.01.08','t5.2020.01.13','t5.2020.01.15'][5:]
 
 #defines the list of all 31 characters and what to call them
 charDef = getHandwritingCharacterDefinitions()
