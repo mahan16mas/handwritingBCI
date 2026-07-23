533e25ba53bbc56d5c10362182ff7220f858e60a
mahan <mahan.48415@gmail.com>
2026-07-23 21:12:09 +0330

    l

--- nlp_lm.py
+++ nlp_lm.py
@@ -102,7 +102,7 @@ for i in range(n):
     for text, lm_state, timesteps, logit_score, lm_score in beams[:N_BEST]:
         scaled_ac_score = logit_score * ACOUSTIC_SCALE
         gpt2_score = compute_gpt2_score(text)
-        final_rescored_total = scaled_ac_score + (2.0 * gpt2_score)
+        final_rescored_total = -scaled_ac_score + (2.0 * gpt2_score)
 
         nbest_results.append({
             'text': text,
