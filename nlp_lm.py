e1653d15f3f2eb49df7077ee7fd2045eed7adf85
mahan <mahan.48415@gmail.com>
2026-07-23 20:13:35 +0330

    l

--- nlp_lm.py
+++ nlp_lm.py
@@ -1,8 +1,29 @@
 import pickle
 import sys, os
 import numpy as np
+import torch
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 from pyctcdecode import build_ctcdecoder
 from characterDefinitions import getHandwritingCharacterDefinitions
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+gpt_tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-xl")
+gpt_model = GPT2LMHeadModel.from_pretrained("gpt2-xl").to(device)
+gpt_model.eval()
+
+def compute_gpt2_score(text):
+    if not text.strip():
+        return 9999.0
+    text_formatted = text[0].upper() + text[1:] if len(text) > 0 else text
+    inputs = gpt_tokenizer(text_formatted, return_tensors="pt").to(device)
+    input_ids = inputs["input_ids"]
+    if input_ids.shape[1] <= 1:
+        return 9999.0
+    with torch.no_grad():
+        outputs = gpt_model(input_ids, labels=input_ids)
+        neg_log_likelihood = outputs.loss.item() * (input_ids.shape[1] - 1)
+    return neg_log_likelihood
+
 labels = [
     "",
     " ",
@@ -14,14 +35,17 @@ labels = [
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
     "u", "v", "w", "x", "y", "z"
 ]
+
 prefix = ''
 charDef = getHandwritingCharacterDefinitions()
 rootDir = "/data/hossein/mm_project" + '/handwritingBCIData/'
-langModelDir = rootDir+'BigramLM'
+langModelDir = rootDir + 'BigramLM'
 i = 0
 folder = f"nlp10-l208-rdpns{i}ro"
+
 with open(f'/data/hossein/mm_project/speech_gru_cebra/{folder}/{prefix}logits', 'rb') as f:
     rnn_outputs = pickle.load(f)
+
 for key in ["logits"]:
     new_list = []
     for item in rnn_outputs[key]:
@@ -36,7 +60,7 @@ BETA = 0.0
 
 decoder = build_ctcdecoder(
     labels=labels,
-    kenlm_model_path=langModelDir  + "/webTextSentences_tokenized-2gram-50000.arpa",
+    kenlm_model_path=langModelDir + "/webTextSentences_tokenized-2gram-50000.arpa",
     alpha=ALPHA,
     beta=BETA
 )
@@ -62,13 +86,14 @@ for i in range(n):
 
     for text, lm_state, timesteps, logit_score, lm_score in beams[:N_BEST]:
         scaled_ac_score = logit_score * ACOUSTIC_SCALE
-        kaldi_total_score = scaled_ac_score + (2.0 * lm_score)
+        gpt2_score = compute_gpt2_score(text)
+        final_rescored_total = scaled_ac_score + (2.0 * gpt2_score)
 
         nbest_results.append({
             'text': text,
             'ac_score': scaled_ac_score,
-            'lm_score': lm_score,
-            'total_score': kaldi_total_score
+            'gpt2_score': gpt2_score,
+            'total_score': final_rescored_total
         })
 
     best_hypothesis = min(nbest_results, key=lambda x: x['total_score'])
