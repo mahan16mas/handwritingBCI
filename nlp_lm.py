034cb0038ff94ce3c312c46185261109bc641479
mahan <mahan.48415@gmail.com>
2026-07-23 20:06:00 +0330

    l

--- nlp_lm.py
+++ nlp_lm.py
@@ -43,31 +43,33 @@ decoder = build_ctcdecoder(
 def softmax(x):
     e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
     return e_x / e_x.sum(axis=-1, keepdims=True)
-logits = rnn_outputs["logits"][0]
-logits = logits[:, charDef['idxToKaldi']]
-if not isinstance(logits, np.ndarray):
-    logits = logits.numpy()
-probs = softmax(logits)
 
-beams = decoder.decode_beams(
-    probs,
-    beam_width=BEAM_WIDTH,
-)
+n = len(rnn_outputs["logits"])
+for i in range(n):
+    logits = rnn_outputs["logits"][0]
+    logits = logits[:, charDef['idxToKaldi']]
+    if not isinstance(logits, np.ndarray):
+        logits = logits.numpy()
+    probs = softmax(logits)
 
-nbest_results = []
+    beams = decoder.decode_beams(
+        probs,
+        beam_width=BEAM_WIDTH,
+    )
 
-for text, lm_state, timesteps, logit_score, lm_score in beams[:N_BEST]:
-    scaled_ac_score = logit_score * ACOUSTIC_SCALE
-    kaldi_total_score = scaled_ac_score + (2.0 * lm_score)
+    nbest_results = []
 
-    nbest_results.append({
-        'text': text,
-        'ac_score': scaled_ac_score,
-        'lm_score': lm_score,
-        'total_score': kaldi_total_score
-    })
+    for text, lm_state, timesteps, logit_score, lm_score in beams[:N_BEST]:
+        scaled_ac_score = logit_score * ACOUSTIC_SCALE
+        kaldi_total_score = scaled_ac_score + (2.0 * lm_score)
 
-best_hypothesis = min(nbest_results, key=lambda x: x['total_score'])
-for hyp in nbest_results:
-    print(hyp['text'])
-print("Best Decoded Text:", best_hypothesis['text'])
+        nbest_results.append({
+            'text': text,
+            'ac_score': scaled_ac_score,
+            'lm_score': lm_score,
+            'total_score': kaldi_total_score
+        })
+
+    best_hypothesis = min(nbest_results, key=lambda x: x['total_score'])
+
+    print("Best Decoded Text:", best_hypothesis['text'])
