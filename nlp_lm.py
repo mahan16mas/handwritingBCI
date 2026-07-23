cd0b39ede3da17d2d357781394b1f98e18abc1bc
mahan <mahan.48415@gmail.com>
2026-07-23 19:59:59 +0330

    l

--- nlp_lm.py
+++ nlp_lm.py
@@ -27,12 +27,18 @@ for key in ["logits"]:
         new_list.append(np.array(item))
     rnn_outputs[key] = new_list
 
+ACOUSTIC_SCALE = 1.79
+BEAM_WIDTH = 65
+N_BEST = 128
+ALPHA = 1.0 / ACOUSTIC_SCALE
+BETA = 0.0
+
 
 decoder = build_ctcdecoder(
     labels=labels,
     kenlm_model_path=langModelDir  + "/webTextSentences_tokenized-2gram-50000.arpa",
-    alpha=0.5,
-    beta=1.0
+    alpha=ALPHA,
+    beta=BETA
 )
 
 def softmax(x):
@@ -45,5 +51,27 @@ if not isinstance(logits, np.ndarray):
     logits = logits.numpy()
 probs = softmax(logits)
 
-decoded_text = decoder.decode(probs)
-print(decoded_text)
+beams = decoder.decode_beams(
+    probs,
+    beam_width=BEAM_WIDTH,
+    prune_log_pt=-10.0
+)
+
+nbest_results = []
+
+for text, lm_state, timesteps, logit_score, lm_score in beams[:N_BEST]:
+    scaled_ac_score = logit_score * ACOUSTIC_SCALE
+
+    kaldi_total_score = scaled_ac_score + (2.0 * lm_score)
+
+    nbest_results.append({
+        'text': text,
+        'ac_score': scaled_ac_score,
+        'lm_score': lm_score,
+        'total_score': kaldi_total_score
+    })
+print(nbest_results)
+
+best_hypothesis = min(nbest_results, key=lambda x: x['total_score'])
+
+print("Best Decoded Text:", best_hypothesis['text'])
