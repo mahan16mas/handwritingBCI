68a4b5a36e0b2f1c938a63c035d7154bfe796e9c
mahan <mahan.48415@gmail.com>
2026-07-23 20:03:19 +0330

    l

--- nlp_lm.py
+++ nlp_lm.py
@@ -4,12 +4,12 @@ import numpy as np
 from pyctcdecode import build_ctcdecoder
 from characterDefinitions import getHandwritingCharacterDefinitions
 labels = [
-    "",    # ایندکس 0 مربوط به <ctc> یا همان CTC Blank است
-    " ",   # ایندکس 1 که جایگزین '>' (فاصله/Space) است
-    "'",   # ایندکس 2
-    ",",   # ایندکس 3
-    ".",   # ایندکس 4 که جایگزین '~' است
-    "?",   # ایندکس 5
+    "",
+    " ",
+    "'",
+    ",",
+    ".",
+    "?",
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
     "u", "v", "w", "x", "y", "z"
